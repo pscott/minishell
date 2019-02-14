010a5b77ef278d4af9a6d05a7eed0c9e4d165ff3
pscott <biliklok@yahoo.fr>
2019-02-15 00:10:19 +0100

    Added expand on setenv ~

--- srcs/builtins/setenv.c
+++ srcs/builtins/setenv.c
@@ -12,6 +12,31 @@
 
 #include "minishell.h"
 
+char	*expand_setenv(char *arg, char **env)
+{
+	int		i;
+	char	*start;
+	char	*tmp;
+	char	*res;
+
+	i = 0;
+	start = ft_strchr(arg, '=');
+	if (start && start[1] == '~')
+	{
+		tmp = get_corresponding_env_setting(ft_strdup("HOME"), env, 0);
+		if (!(res= ft_strnew(ft_strlen(arg) - 1 + ft_strlen(tmp))))
+			ERR_MEM;
+		ft_strncpy(res, arg, start - arg + 1);
+		ft_strncpy(res + (start - arg + 1), tmp, ft_strlen(tmp));
+		ft_strncpy(res + (start - arg + 1) + ft_strlen(tmp), arg + (start - arg)
+				+ 2, ft_strlen(start) - 1);
+		ft_memdel((void*)&arg);
+		ft_memdel((void*)&tmp);
+		return (res);
+	}
+	return (arg);
+}
+
 void	add_envs(char **argv, char **env)
 {
 	unsigned int	i;
@@ -19,6 +44,7 @@ void	add_envs(char **argv, char **env)
 	i = 0;
 	while ((is_env_setting(argv[++i])))
 	{
+		argv[i] = expand_setenv(argv[i], env);
 		if (setting_is_in_env(argv[i], env) == 0)
 			append_new_env(argv[i], env);
 		else
